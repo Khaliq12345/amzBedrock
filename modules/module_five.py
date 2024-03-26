317fb095c32ffb876dbac44f46ae7aba247b6b08
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2024-03-26 09:57:37 +0000

    changed pages name and added module 5

---
+++ modules/module_five.py
@@ -0,0 +1,234 @@
+import pandas as pd
+import dateparser
+import pandas
+from datetime import datetime
+import gdown
+from warnings import filterwarnings
+
+filterwarnings('ignore')
+
+out_cols = ['Product',
+ 'Entity',
+ 'Operation',
+ 'Campaign ID',
+ 'Portfolio ID',
+ 'Ad Group ID',
+ 'Ad ID',
+ 'Targeting ID',
+ 'Campaign Name',
+ 'Ad Group Name',
+ 'Start Date',
+ 'End Date',
+ 'State',
+ 'Tactic',
+ 'Budget Type',
+ 'Budget',
+ 'SKU',
+ 'ASIN',
+ 'Ad Group Default Bid',
+ 'Bid',
+ 'Bid Optimization',
+ 'Cost Type',
+ 'Targeting Expression',
+]
+
+def get_table(cols: list, target_type:str, targets:list, asin=False):
+    if asin:
+        items = []
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': 'Campaign'
+        })
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': 'Ad Group'
+        })
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': 'Product Ad'
+        })
+        for target in targets:
+            items.append({
+                'Product': "Sponsored Display",
+                'Entity': target_type
+            })        
+    else:
+        items = []
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': 'Campaign'
+        })
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': 'Ad Group'
+        })
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': 'Product Ad'
+        })
+        items.append({
+            'Product': "Sponsored Display",
+            'Entity': target_type
+        })   
+    df = pd.DataFrame(items, columns=cols)
+    return df
+
+def get_targets(file_url, type_):
+    if type_ == 'list':
+        output = "target.txt"
+        gdown.download(file_url, output, fuzzy=True, quiet=True)
+        with open('target.txt', 'r') as f:
+            trgs = f.readlines()
+            trgs = [t.replace('\n', '').replace('\ufeff', '') for t in trgs]
+        return trgs
+    elif type_ == 'df':
+        output = 'target.csv'
+        gdown.download(file_url, output, fuzzy=True, quiet=True)
+        df = pd.read_csv('target.csv', header=None)
+        return df[0].to_list(), df[1].to_list()
+    else:
+        print('Please check your type: The only supported types are list and df')
+
+def processing_targets(row):
+    asins = targetings = categories = None
+    if (row['Targeting'] == 'Contextual Targeting') and (row['Contextual Targeting'] == 'asin'):
+        asins = get_targets(row['Contextual Targeting Targeting File'], 'list')
+        return asins
+    elif (row['Targeting'] == 'Contextual Targeting') and (row['Contextual Targeting'] == 'category'):
+        targetings, categories = get_targets(row['Contextual Targeting Targeting File'], 'df')
+        return targetings, categories
+    elif (row['Targeting'] == 'Audience Targeting') and ('Category' in row['Audience Targeting']):
+        targetings, categories = get_targets(row['Audience Targeting Targeting File'], 'df')
+        return targetings, categories
+
+def parse_date(date_str):
+    adder = lambda x: f'0{x}' if len(x) < 2 else x
+    parse_date = dateparser.parse(date_str)
+    year = parse_date.year
+    month = adder(str(parse_date.month))
+    day = adder(str(parse_date.day))
+    return f'{year}{month}{day}'
+
+def common_df_processor(x_table, campaign_name, row):
+    x_table['Campaign Name'] = campaign_name  
+    x_table['Campaign ID'] = campaign_name
+    x_table['Portfolio ID'] = row['Portfolio ID']
+    x_table.loc[x_table['Entity'] != 'Campaign', 'Ad Group ID'] = campaign_name
+    x_table.loc[x_table['Entity'] == 'Ad Group', 'Ad Group Name'] = campaign_name
+    #date
+    x_table.loc[x_table['Entity'] == 'Campaign', 'Start Date'] = parse_date(str(row['Start Date']))
+    #state
+    enable_pause = lambda x: 'enabled' if x == 'yes' else 'paused'
+    x_table.loc[x_table['Entity'].isin(['Campaign', 'Ad Group']), 'State'] = enable_pause(row['Activate Campaign and Ad Group'])
+    x_table.loc[~(x_table['Entity'].isin(['Campaign', 'Ad Group'])), 'State'] = 'enabled'
+    #Budget
+    x_table.loc[x_table['Entity'] == 'Campaign', 'Budget Type'] = 'daily'
+    x_table.loc[x_table['Entity'] == 'Campaign', 'Budget'] = row['Daily Budget']
+    #sku and asin
+    console_sc_vc = lambda x, y, value: y if x == value else None
+    x_table.loc[x_table['Entity'] == 'Product Ad', 'SKU'] = console_sc_vc(row['Console'], row['SKU'], 'SC')
+    x_table.loc[x_table['Entity'] == 'Product Ad', 'ASIN'] = console_sc_vc(row['Console'], row['ASIN'], 'VC')
+    #Bid
+    x_table.loc[x_table['Entity'] == 'Ad Group', 'Ad Group Default Bid'] = row['Bid']
+    #Bid optimization
+    x_table.loc[x_table['Entity'] == 'Ad Group', 'Bid Optimization'] = row['Bid Optimization']
+    return x_table
+
+def proccess_df(input_df: pd.DataFrame):
+    input_df.dropna(how='all')
+    dfs = []
+    input_df['Portfolio ID'] = input_df['Portfolio ID'].astype(str)
+    for i, row in input_df.iterrows():
+        #option 1
+        if (row['Targeting'] == 'Contextual Targeting') and (row['Contextual Targeting'] == 'asin'):
+            targets = processing_targets(row)
+            x_table = get_table(out_cols, 'Contextual Targeting', targets, asin=True)
+            x_table['Operation'] = 'create'
+            #cost type
+            cost_type_selector = lambda x: 'cpc' if (x == 'Optimize for page visits') or (x == 'Optimize for conversions') else 'vcpm'
+            x_table.loc[x_table['Entity'] == 'Campaign', 'Cost Type'] = cost_type_selector(row['Bid Optimization'])
+            #targeting expression
+            targeting_exp_values = [f'asin="{t}"' for t in targets]
+            x_table.loc[x_table['Entity'] == 'Contextual Targeting', 'Targeting Expression'] = targeting_exp_values
+            #campaign name
+            cost_type = cost_type_selector(row['Bid Optimization'])
+            if row['Defense'] == 'yes':
+                campaign_name = f"SD_{row['SKU']}_{row['ASIN']}_{cost_type}_ASIN Targeting_Defense"
+            else:
+                campaign_name = f"SD_{row['SKU']}_{row['ASIN']}_{cost_type}_ASIN Targeting"
+            x_table.loc[x_table['Entity'] == 'Campaign', 'Tactic'] = 'T00020'
+            x_table = common_df_processor(x_table, campaign_name, row)
+            dfs.append(x_table)
+
+        #option 2
+        elif (row['Targeting'] == 'Contextual Targeting') and (row['Contextual Targeting'] == 'category'):
+            targets = processing_targets(row)
+            for cat, cat_name in zip(targets[0], targets[1]):
+                x_table = get_table(out_cols, 'Contextual Targeting', [], asin=False)
+                x_table['Operation'] = 'create'
+                #cost type
+                cost_type_selector = lambda x: 'cpc' if (x == 'Optimize for page visits') or (x == 'Optimize for conversions') else 'vcpm'
+                x_table.loc[x_table['Entity'] == 'Campaign', 'Cost Type'] = cost_type_selector(row['Bid Optimization'])
+                #targeting expression
+                targeting_exp_value = f'category="{cat}"'
+                x_table.loc[x_table['Entity'] == 'Contextual Targeting', 'Targeting Expression'] = targeting_exp_value
+                #campaign name
+                cost_type = cost_type_selector(row['Bid Optimization'])
+                if row['Defense'] == 'yes':
+                    campaign_name = f"SD_{row['SKU']}_{row['ASIN']}_{cost_type}_{cat_name}_Defense"
+                else:
+                    campaign_name = f"SD_{row['SKU']}_{row['ASIN']}_{cost_type}_{cat_name}"
+                x_table.loc[x_table['Entity'] == 'Campaign', 'Tactic'] = 'T00020'
+                x_table = common_df_processor(x_table, campaign_name, row)
+                dfs.append(x_table)
+
+        # option 3
+        elif (row['Targeting'] == 'Audience Targeting') and (('Category' in row['Audience Targeting'])):
+            targets = processing_targets(row)
+            for cat, cat_name in zip(targets[0], targets[1]):
+                x_table = get_table(out_cols, 'Audience Targeting', [], asin=False)
+                x_table['Operation'] = 'create'
+                #cost type
+                cost_type_selector = lambda x: 'cpc' if (x == 'Optimize for page visits') or (x == 'Optimize for conversions') else 'vcpm'
+                x_table.loc[x_table['Entity'] == 'Campaign', 'Cost Type'] = cost_type_selector(row['Bid Optimization'])
+                #targeting expression
+                if 'views-Category' in row['Audience Targeting']:
+                    targeting_exp_value = f"views-category-[{cat}]-lookback-{row['Lookback']}"
+                elif 'purchases-Category' in row['Audience Targeting']:
+                    targeting_exp_value = f"purchases-category-[{cat}]-lookback-{row['Lookback']}"
+                else:
+                    targeting_exp_value = None
+                x_table.loc[x_table['Entity'] == 'Audience Targeting', 'Targeting Expression'] = targeting_exp_value
+                #campaign name
+                cost_type = cost_type_selector(row['Bid Optimization'])
+                campaign_name = f"SD_{row['SKU']}_{row['ASIN']}_{cost_type}_{targeting_exp_value}_Retargeting"
+                x_table.loc[x_table['Entity'] == 'Campaign', 'Tactic'] = 'T00030'
+                x_table = common_df_processor(x_table, campaign_name, row)
+                dfs.append(x_table)
+        
+        #option 4
+        elif (row['Targeting'] == 'Audience Targeting') and (('Category' not in row['Audience Targeting'])):
+            x_table = get_table(out_cols, 'Audience Targeting', [], asin=False)
+            x_table['Operation'] = 'create'
+            #cost type
+            cost_type_selector = lambda x: 'cpc' if (x == 'Optimize for page visits') or (x == 'Optimize for conversions') else 'vcpm'
+            x_table.loc[x_table['Entity'] == 'Campaign', 'Cost Type'] = cost_type_selector(row['Bid Optimization'])
+            #targeting expression
+            if 'views-Advertised products' in row['Audience Targeting']:
+                targeting_exp_value = f"views-exact-product lookback-{row['Lookback']}"
+            elif 'views-Related to advertised products' in row['Audience Targeting']:
+                targeting_exp_value = f"views-similar-product lookback-{row['Lookback']}"
+            elif 'purchases-Advertised products' in row['Audience Targeting']:
+                targeting_exp_value = f"purchases-exact-product lookback-{row['Lookback']}"
+            elif 'views-Related to advertised products' in row['Audience Targeting']:
+                targeting_exp_value = f"purchases-related-product lookback-{row['Lookback']}"
+            x_table.loc[x_table['Entity'] == 'Audience Targeting', 'Targeting Expression'] = targeting_exp_value
+            #campaign name
+            cost_type = cost_type_selector(row['Bid Optimization'])
+            campaign_name = f"SD_{row['SKU']}_{row['ASIN']}_{cost_type}_{targeting_exp_value}_Retargeting"
+            x_table.loc[x_table['Entity'] == 'Campaign', 'Tactic'] = 'T00030'
+            x_table = common_df_processor(x_table, campaign_name, row)
+            dfs.append(x_table)
+
+    output_dataframe = pd.concat(dfs, ignore_index=True)
+    return output_dataframe
---
+++ pages/5_ SD Product DP.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from modules import module_five
+import pandas as pd
+import io
+from streamlit_extras.colored_header import colored_header
+
+buffer = io.BytesIO()
+
+st.set_page_config(
+    page_title="Module 5 -  SD Product DP",
+    page_icon="🏢",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+colored_header(
+    label='SD Product DP',
+    color_name= 'red-70'
+)
+
+if 'access' not in st.session_state:
+    st.session_state['access'] = False
+if 'module_name' not in st.session_state:
+    st.session_state['module_name'] = None
+if 'username' not in st.session_state:
+    st.session_state['username'] = None
+if 'date' not in st.session_state:
+    st.session_state['date'] = None
+
+st.session_state['module_name'] = 'SD Product DP'
+
+def module_5():
+    output_dataframe = pd.DataFrame()
+
+    with st.form('form-1', border=True):
+        data_file = st.file_uploader('Upload your excel data', type=['xlsx'])
+        with st.expander('Look at what your Input data looks like'):
+            if data_file is not None:
+                input_df = pd.read_excel(data_file).dropna(how='all')
+                st.table(input_df.head())
+                output_dataframe = module_four.proccess_df(input_df)
+        st.form_submit_button('Start Processing')
+
+    if not output_dataframe.empty:
+        with st.expander('Look at what your Output data looks like'):
+            st.table(output_dataframe.head())
+                
+        writer = pd.ExcelWriter(buffer, engine='xlsxwriter')
+        output_dataframe.to_excel(writer, sheet_name='Sheet1', index=False)
+        writer.close()
+        st.download_button(
+            label="Download Output",
+            data=buffer,
+            file_name=f"Output_{st.session_state['module_name']}_{st.session_state['username']}_{st.session_state['date']}.xlsx",
+            mime="application/vnd.ms-excel"
+        )
+
+if st.session_state['access']:
+    module_5()
+else:
+    st.warning('Go to Homepage to login before you can access any module')
